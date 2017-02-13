8877993768f5c762473fcf89d6f8a6e7b29efb0c
Mehidine <mehidine.chupeau@etudiant.univ-rennes1.fr>
2017-02-13 14:41:25 +0100

    first commit

---
+++ src/main/java/fr/esir/App.java
@@ -0,0 +1,15 @@
+package fr.esir;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        //Ca commence ici
+        System.out.println( "Hello World!" );
+        //Ca termine ici
+    }
+}
